40b50c3c8dd47934b4a31d03542cf7a7f700ce8d
Suzy Yang <minus0805@gmail.com>
2020-07-22 17:53:34 +0900

    Updates

---
+++ precache-manifest.6b15c19176d9ddde583c8ca26388e89d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1fbdbfd31ff0f1ec9ba3667c310a883",
+    "url": "/minus0805/movie-app/index.html"
+  },
+  {
+    "revision": "8a3e05f2afe853608930",
+    "url": "/minus0805/movie-app/static/css/main.17c58945.chunk.css"
+  },
+  {
+    "revision": "a4987c1aed0faee1d8f9",
+    "url": "/minus0805/movie-app/static/js/2.4c35eb24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minus0805/movie-app/static/js/2.4c35eb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3e05f2afe853608930",
+    "url": "/minus0805/movie-app/static/js/main.b0faaf85.chunk.js"
+  },
+  {
+    "revision": "24b99b254fc72cbad59f",
+    "url": "/minus0805/movie-app/static/js/runtime-main.380e1c54.js"
+  }
+]);
